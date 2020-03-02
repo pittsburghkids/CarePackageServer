c772031d8a7883821c88bcaab52940ca48554dd6
Scott Garner <scott@j38.net>
2020-03-02 09:31:39 -0500

    Hard code URL for now.

--- Node/CarePackageLocal/lib/socketIOClient.js
+++ Node/CarePackageLocal/lib/socketIOClient.js
@@ -5,14 +5,13 @@ class SocketIOClient extends EventEmitter {
     constructor() {
         super();
 
-        this.socket = require('socket.io-client')('http://localhost:4000');
+        this.socket = require('socket.io-client')('http://carepackagedepot.local:4000');
 
         this.socket.on('connect', () => {
             console.log("socket.io connect");
         });
 
         this.socket.on('data', (data) => {
-            console.log(data);
             this.emit("data", data);
         });
 
