d502df52b9dded2efb291dbf8922cc455828782b
Scott Garner <scott@j38.net>
2020-03-02 11:43:37 -0500

    Eternal title updatesl

--- Node/CarePackageLocal/start.ubuntu.sh
+++ Node/CarePackageLocal/start.ubuntu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/x-terminal-emulator -e echo -e '\033]2;CarePackageServer\007' && /usr/bin/npm --prefix=/home/exhibits/Desktop/CarePackageServer/Node/CarePackageLocal/ run dev
+/usr/bin/x-terminal-emulator -e echo -e '\033]2;CarePackageServer\007'; /usr/bin/npm --prefix=/home/exhibits/Desktop/CarePackageServer/Node/CarePackageLocal/ run dev
