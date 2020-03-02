599e39b62952893f83a1f6b28d81cd712014fdf5
Scott Garner <scott@j38.net>
2020-03-02 11:00:20 -0500

    SH header for startup scripts

--- Node/CarePackageLocal/start.ubuntu.sh
+++ Node/CarePackageLocal/start.ubuntu.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 /usr/bin/x-terminal-emulator -e /usr/bin/npm --prefix=/home/exhibits/Desktop/CarePackageServer/Node/CarePackageLocal/ run dev
--- Node/CarePackageMaster/start.ubuntu.sh
+++ Node/CarePackageMaster/start.ubuntu.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 /usr/bin/x-terminal-emulator -e /usr/bin/npm --prefix=/home/exhibits/Desktop/CarePackageServer/Node/CarePackageMaster/ run dev
